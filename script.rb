c85a096765d4c2adb8dbb4be31e925f422f496a9
Georjane <60772732+Georjane@users.noreply.github.com>
2020-06-20 09:32:13 +0100

    Use .zero method and remove unnecessary i assignment

--- script.rb
+++ script.rb
@@ -1,16 +1,16 @@
 def my_fizzbuzz
-  puts "please enter the number you want to FizzBuzz to:"
+  puts 'please enter the number you want to FizzBuzz to:'
   x = gets.chomp.to_i
   arr = (1..x).to_a
   game = arr.map do |i|
-    if i % 3 == 0 && i % 5 == 0
-    i = "fizzbuzz"
-    elsif i % 3 == 0
-    i = "fizz"
-    elsif i % 5 == 0
-    i = "buzz"
+    if (i % 3).zero? && (i % 5).zero?
+      "fizzbuzz"
+    elsif (i % 3).zero?
+      "fizz"
+    elsif (i % 5).zero?
+      "buzz"
     else
-    i
+      i
     end
   end
   puts game
